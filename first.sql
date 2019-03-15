62a80b01166522793d621c62d62acadd2bf58402
abakanova <48572295+abakanova@users.noreply.github.com>
2019-03-15 11:42:46 +0300

    Update first.sql

--- first.sql
+++ first.sql
@@ -1,6 +1,6 @@
 SELECT count(call_id)*0.04 as 'expenses' from newschema.call_logs, newschema.numbers  
-where call_logs.UID=numbers.UID and call_dir='out';
+where call_logs.To!=numbers.Phone_Number and call_dir='out';
 
 SELECT count(call_id)*0.04 as 'expenses' from newschema.call_logs 
-join newschema.numbers on call_logs.UID=numbers.UID
+join newschema.numbers on call_logs.To!=numbers.Phone_Number
 where call_dir='out';
