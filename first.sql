77ebf69e7dab3d3a954d018d952925f15ac16e5f
abakanova <48572295+abakanova@users.noreply.github.com>
2019-03-18 02:32:24 +0300

    Update first.sql

--- first.sql
+++ first.sql
@@ -1,13 +1,38 @@
-SELECT count(call_id)*0.04 as 'expenses1' from newschema.call_logs, newschema.numbers  
-where call_logs.To!=numbers.Phone_Number and call_dir='out';
+SELECT * FROM newschema.call_logs;
 
-SELECT count(call_id)*0.04 as 'expenses1' from newschema.call_logs 
-join newschema.numbers on call_logs.To!=numbers.Phone_Number
-where call_dir='out';
+SELECT count(*) from newschema.call_logs;
 
-SELECT count(call_id)*0.04 as 'expenses2' from newschema.call_logs, newschema.numbers  
-where call_logs.From!=numbers.Phone_Number and call_dir='out';
+SELECT * FROM newschema.call_logs where call_dir='out';
 
-SELECT count(call_id)*0.04 as 'expenses2' from newschema.call_logs 
-join newschema.numbers on call_logs.From!=numbers.Phone_Number
-where call_dir='out';
+SELECT count(*) FROM newschema.call_logs where call_dir='out';
+
+SELECT * FROM call_logs log, call_forwarding forw 
+where log.From=forw.From and log.To=forw.To;
+
+SELECT * FROM call_logs log, call_forwarding forw 
+where log.From=forw.From and log.To=forw.To and call_dir='out';
+
+SELECT count(*) FROM call_logs log, call_forwarding forw 
+where log.From=forw.From and log.To=forw.To GROUP BY log.UID;
+-- group by UID
+SELECT count(*), log.UID FROM call_logs log
+ GROUP BY log.UID;
+
+select count(*) from call_logs
+join numbers on call_logs.To=numbers.Phone_Number
+where call_dir='out' and numbers.UID;
+
+
+select count(*) from call_logs
+left join numbers on call_logs.To=numbers.Phone_Number
+where call_dir='out' and numbers.UID;
+
+select count(*) from call_logs
+left join numbers on call_logs.To=numbers.Phone_Number
+where call_dir='out' and numbers.UID is NULL;
+
+select count(*)*0.04 as 'expenses' from call_logs
+left join numbers on call_logs.To=numbers.Phone_Number
+where call_dir='out' and numbers.UID is NULL;
+ 
+ 
