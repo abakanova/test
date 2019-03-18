fd836e74e27c08e8708d423e6f9e0cc913a8b0d2
abakanova <48572295+abakanova@users.noreply.github.com>
2019-03-18 14:44:13 +0300

    Update second.sql

--- second.sql
+++ second.sql
@@ -7,5 +7,6 @@ Select count(*) cnt, call_logs.UID, call_logs.Call_dir, accounts.Name from call_
 join accounts on call_logs.UID=accounts.UID group by call_dir,UID order by cnt desc LIMIT 10;
 
 -- users with the longest calls
-Select call_logs.Call_id,call_logs.Timestamp_start, call_logs.Timestamp_end, call_logs.Timestamp_end-call_logs.Timestamp_start as 'duration', call_logs.UID,accounts.name 
+Select call_logs.Call_id,call_logs.Timestamp_start, call_logs.Timestamp_end, 
+call_logs.Timestamp_end-call_logs.Timestamp_start as 'duration', call_logs.UID,accounts.name 
 from call_logs join accounts on call_logs.UID=accounts.UID order by duration desc limit 10;
